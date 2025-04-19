9e178c9dfc17f3b463fd13c490b3e51bbe7692f5
m3dry <m3dry@tutamail.com>
2025-04-19 21:00:14 +0200

    feat: hub background art

--- clang_config.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-included=$(g++ -v -c -xc++ /dev/null 2>&1 | sed -n '/#include <...> search starts here:/, /End of search list./p' | sed '1d;$d ; s/ */-I/' | paste -sd "," -)
-
-printf "CompileFlags:\n\tAdd: [-isystem,%s]\n" "$included" > .clangd
